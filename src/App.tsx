70cfc45d0a98456b5939b73eec624a6cc25b338c
Suyal Sahukhal <suyalsahukhala787@gmail.com>
2026-07-12 23:07:37 +0545

    feat: wire up routing, API client, React Query, shared types

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { Routes, Route } from "react-router-dom";
+import Layout from "@components/Layout";
+import ProductsPage from "@pages/ProductsPage";
+import HomePage from "@pages/HomePage";
+import ProductDetailPage from "@pages/ProductsPage";
+import CartPage from "@pages/CartPage";
+import NotFoundPage from "@pages/NotFoundPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<HomPage />} />
+        <Route index element={<HomePage />} />
         <Route path="products" element={<ProductsPage />} />
         <Route path="products/ :id" element={<ProductDetailPage />} />
         <Route path="cart" element={<CartPage />} />
